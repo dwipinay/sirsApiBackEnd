980a3d5ff530e62c8ed9da07671900d8a74dc290
dwip <cumadwip@gmail.com>
2023-02-01 17:54:15 +0800

    work in progress

--- config/Database.js
+++ config/Database.js
@@ -12,7 +12,7 @@ export const databaseSIRS = new Sequelize(process.env.DB_DATABASE_SIRS, process.
 	connectTimeout: 60000
     },
     timezone: '+07:00', //for writing to database
-    logging: false,
+    logging: true,
     pool: {
         max: 100,
         min: 0,
